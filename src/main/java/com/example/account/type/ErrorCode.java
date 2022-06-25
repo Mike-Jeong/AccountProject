65859aa6c32bbb2b8d38f422ad4fb85d66c6b35f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-26 02:50:34 +0900

    계좌해지 API 관련 dto 및 method 생성

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     USER_NOT_FOUND("사용자가 없습니다."),
-    Max_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다");
+    Max_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다"),
+    ACCOUNT_ALREADY_UNREGISTERED("계좌가 이미 해지되었습니"),
+    BALANCE_NOT_EMPTY("잔액이 있는 계좌는 해지 할 수 없습니다"),
+    USER_ACCOUNT_UN_MATCH("사용자와 계좌 소유주가 다릅니다.");
 
     private final String description;
 }
