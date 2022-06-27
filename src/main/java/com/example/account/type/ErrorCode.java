cb2e25ea1bdda1b22362204c9c4b3d64edb3551d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-27 15:07:13 +0900

    일관성 있는 예외처리를 위한 ErrorHandler 작성

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+
+    INTERNAL_SERVER_ERROR("내부 서버 오류."),
     INVALID_REQUEST("잘못된 요청 입니다."),
     USER_NOT_FOUND("사용자가 없습니다."),
     Max_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다."),
